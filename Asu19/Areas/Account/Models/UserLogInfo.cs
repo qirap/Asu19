43c8ba84eb302b0c8070fd79bcb10e15b14b00db
Anton <qi_ra@vk.com>
2022-11-08 17:23:45 +0500

    Исправил валидацию и изменение данных пользователя

--- Asu19/Areas/Account/Models/UserLogInfo.cs
+++ Asu19/Areas/Account/Models/UserLogInfo.cs
@@ -1,13 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace Asu19.Areas.Account.Models
 {
     public class UserLogInfo : IUserAuthInfo
     {
-        [Required]
+        [Required (ErrorMessage = "Введите логин")]
+        [StringLength(32, MinimumLength = 4, ErrorMessage = "Длина логина должна быть от 4 до 32 символов")]
         public string? Login { get; set; }
 
-        [Required]
+        [Required (ErrorMessage = "Введите пароль")]
+        [StringLength(32, MinimumLength = 4, ErrorMessage = "Длина пароля должна быть от 4 до 32 символов")]
         public string? Password { get; set; }
     }
 }
--- Asu19/Areas/Account/Models/UserRegInfo.cs
+++ Asu19/Areas/Account/Models/UserRegInfo.cs
@@ -4,19 +4,24 @@ namespace Asu19.Areas.Account.Models
 {
     public class UserRegInfo : IUserAuthInfo
     {
-        [Required]
+        [Required(ErrorMessage = "Введите логин")]
+        [StringLength(32, MinimumLength = 4, ErrorMessage = "Длина логина должна быть от 4 до 32 символов")]
         public string? Login { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Введите пароль")]
+        [StringLength(32, MinimumLength = 4, ErrorMessage = "Длина пароля должна быть от 4 до 32 символов")]
         public string? Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Введите имя")]
+        [StringLength(32, MinimumLength = 4, ErrorMessage = "Длина имени должна быть от 4 до 32 символов")]
         public string? FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Введите фамилию")]
+        [StringLength(32, MinimumLength = 4, ErrorMessage = "Длина фамилии должна быть от 4 до 32 символов")]
         public string? LastName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Введите адрес")]
+        [StringLength(32, MinimumLength = 4, ErrorMessage = "Длина адреса должна быть от 4 до 32 символов")]
         public string? Address { get; set; }
     }
 }
